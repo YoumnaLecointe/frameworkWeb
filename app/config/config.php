66d256cf6d1605e7e83b141d7ebaff1cad668751
Lecointe <lecointey@laposte.net>
2021-03-08 16:07:30 +0100

    Pt 4

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@ return array(
 		"siteUrl"=>"http://127.0.0.1/tds/",
 		"database"=>[
 				"type"=>"mysql",
-				"dbName"=>"",
+				"dbName"=>"store",
 				"serverName"=>"127.0.0.1",
 				"port"=>"3306",
 				"user"=>"root",
