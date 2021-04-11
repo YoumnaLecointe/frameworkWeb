a224859b3b5e8bf2d0948d33efae116e80e6b416
Lecointe <lecointey@laposte.net>
2021-04-11 23:24:31 +0200

    add route product

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -4,6 +4,7 @@ namespace controllers;
  use models\Basket;
  use models\Order;
  use models\Product;
+ use models\Section;
  use services\dao\OrgaRepository;
  use Ubiquity\controllers\auth\AuthController;
  use Ubiquity\controllers\auth\WithAuthTrait;
@@ -38,36 +39,62 @@ class MainController extends ControllerBase{
         $this->repo = $repo;
     }
 
+    //Chargement page index
     public function index() {
 	    $this->loadView("MainController/index.html");
     }
 
+    //Route des commandes
     #[Route ('order', name:'order')]
     public function orders(){
         $orders = DAO::getAll(Order::class, 'idUser= ?', false, [USession::get("idUser")]);
         $this->loadDefaultView(['orders'=>$orders]);
     }
 
+    //Route du store
     #[Route ('store', name:'store')]
     public function store(){
         $store = DAO::getAll(Product::class, false, false);
-        $this->loadDefaultView(['store'=>$store]);
+        $listsections = DAO::getAll(Section::class,'', ['products']);
+        $listProm = DAO::getAll(Product::class, 'promotion< ?', false, [0]);
+        //$nbprodSection = DAO::count(Product::class, 'section= ?', false?);
+        $this->loadDefaultView(['store'=>$store, 'listProm'=>$listProm, 'listSection'=>$listsections]);
     }
-
     protected function getAuthController(): AuthController
     {
         return new MyAuth($this);
     }
 
+    //Creation du panier
     #[Route ('basket', name:'basket')]
     public function basket(){
         $baskets = DAO::getAll(Basket::class, 'idUser= ?', false, [USession::get("idUser")]);
         $this->loadDefaultView(['baskets'=>$baskets]);
     }
 
+    //Route de l'ajout de panier
     #[Route ('newBasket', name:'newBasket')]
     public function newBasket(){
         $newbasket = DAO::getAll(Order::class, 'idUser= ?', false, [USession::get("idUser")]);
         $this->loadDefaultView(['newbasket'=>$newbasket]);
+    }
+
+    //La route pour les sections
+    #[Route ('section/{id}', name:'section')]
+    public function section($id){
+        $product = DAO::getAll(Product::class, 'idSection= '.$id, [USession::get("idSection")]);
+        $section = DAO::getById(Section::class,$id,['products']);
+        $listsections = DAO::getAll(Section::class,'', ['products']);
+        $this->loadDefaultView(['section'=>$section, 'listSection'=>$listsections, 'product'=>$product]);
+    }
+
+    //Route produit
+    #[Route ('product/{idSection}/{idProduct}', name:'product')]
+    public function product($idSection,$idProduct){
+        $product = DAO::getAll(Product::class, 'idSection= '.$idProduct, [USession::get("idSection")]);
+        $productid = DAO::getById(Product::class,$idProduct,['sections']);
+        $section = DAO::getById(Section::class,$idSection,['products']);
+        $listsections = DAO::getAll(Section::class,'', ['products']);
+        $this->loadDefaultView(['section'=>$section, 'listSection'=>$listsections, 'product'=>$product, 'productid'=>$productid]);
     }
 }
